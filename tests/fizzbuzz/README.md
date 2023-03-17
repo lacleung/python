2a108b65e5d4f1c426feb4b492144ba98081a657
lacleung <lacleung@gmail.com>
2023-03-17 12:25:29 -0700

    DEV: move files and apply style guide

---
+++ tests/fizzbuzz/README.md
@@ -0,0 +1,8 @@
+# Fizzbuzz
+
+From [wikipedia](https://en.wikipedia.org/wiki/Fizz_buzz): "Fizz buzz (often spelled FizzBuzz in this context) has been used as an interview screening device for computer programmers. Writing a program to output the first 100 FizzBuzz numbers is a relatively trivial problem requiring little more than a loop and conditional statements. However, its value in coding interviews is to analyze fundamental coding habits that may be indicative of overall coding ingenuity."
+
+---
+
+## Solution 
+This simple solution is just a python class with a variable input to return a comma sepearated sequence to solve the fizz buzz challenge.
--- tests/fizzbuzz/main.py
+++ tests/fizzbuzz/main.py
@@ -1,19 +1,24 @@
- 
 class Solution:
 
-	def run(self, N, M):
-		seq = list(range(N,M+1))
-		fb_seq = []
-		sequence = ""
+   def run(N, M):
+       seq = list(range(N,M+1))
+       fb_seq = []
+       sequence = ""
+       for i in range(0,len(seq)):
+           fb_seq.append('')
+           if seq[i]%3 == 0:
+               fb_seq[i] += "fizz"
+           if seq[i]%5 == 0:
+               fb_seq[i] += "buzz"
+           if fb_seq[i] == '':
+               fb_seq[i] = seq[i]
+   
+       sequence = ', '.join([str(elem) for elem in fb_seq])
+       return sequence
 
-		for i in range(0,len(seq)):
-			fb_seq.append('')
-			if seq[i]%3 == 0:
-				fb_seq[i] += "fizz"
-			if seq[i]%5 == 0:
-				fb_seq[i] += "buzz"
-			if fb_seq[i] == '':
-				fb_seq[i] = seq[i]
-	
-		sequence = ','.join([str(elem) for elem in fb_seq])
-		return sequence
+n = int(input('Enter the beginning of range: '))
+m = int(input('Enter end of range: '))
+
+s = Solution.run(n, m)
+
+print (s)
